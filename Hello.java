c5908dab9dd933da4fdd0835d471e9184870fbf9
Pinaki Ranjan Das <pinaki.ranjan@northsouth.edu>
2021-03-24 11:00:53 +0600

    Added a new methos called display to hello .java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+ public static void main (String[] args){
+
+     System.out.println ("Vs code is Awesome...");
+
+     greeting ("HM Nayem");
+ }
+public static void greeting(String name){
+
+    System.out.println("Welcome " + name);
+}
+
+}
